dc753a7f7c15b6b242dbe4be1b06b76e181da295
Michael Silvestre <willtard@gmail.com>
2015-06-09 21:00:28 +0200

    Reorganize the module

--- example/app.js
+++ example/app.js
@@ -1,57 +1,49 @@
-'use strict';
+var ShifterbeltClient = require('../lib/ShiflterbeltClient.js');
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-var _ShiflterbeltClientJs = require('../ShiflterbeltClient.js');
-
-var _ShiflterbeltClientJs2 = _interopRequireDefault(_ShiflterbeltClientJs);
-
-var shifterbeltClient = new _ShiflterbeltClientJs2['default']({
-  url: String(process.argv.slice(2, 3)[0]),
+var shifterbeltClient = new ShifterbeltClient({
+  url: String(process.argv.slice(2,3)[0]),
   applicationId: Number(process.argv.slice(3, 4)[0]),
   key: process.argv.slice(4, 5)[0],
   password: process.argv.slice(5, 6)[0],
   macAddress: process.argv.slice(6).toString()
 });
 
-shifterbeltClient.on('connect', function (socket) {
-  console.log('connected on Shifterbelt');
+shifterbeltClient.on('connect', function(socket) {
+  console.log("connected on Shifterbelt");
   var count = 0;
-  if (socket.role === 'slave') {
-    setInterval(function () {
-      socket.emit('test', 'first device message, ' + count++);
+  if (socket.role === "slave") {
+    setInterval(function() {
+      socket.emit('test', "first device message, " +  (count++) );
     }, 3000);
   }
 
-  if (socket.role === 'master') {
-    setInterval(function () {
-      socket.emit('test', 'b8e8563aa597', 'first device message, ' + count++);
+  if (socket.role === "master") {
+    setInterval(function() {
+      socket.emit('test', 'MAC_ADDRESS_OF_THE_SLAVE', 'first device message, ' + (count++));
     }, 3000);
   }
 
-  socket.on('event', function (message) {
+  socket.on('event', function(message) {
     console.log('message: ' + message);
   });
 
-  if (socket.role === 'slave') {
-    socket.on('test', function (message) {
-      console.log('message: ' + message);
+  if (socket.role === "slave") {
+    socket.on('test', function(message) {
+      console.log('message: '+ message);
     });
   }
 
-  if (socket.role === 'master') {
-    socket.on('test', function (sender, message) {
+  if (socket.role === "master") {
+      socket.on('test', function(sender, message) {
       console.log('sender: ' + sender + ', message: ' + message);
     });
   }
 
-  socket.on('disconnect', function () {
-    console.log('has been disconnected');
+  socket.on('disconnect', function() {
+    console.log("has been disconnected");
   });
 });
 
-shifterbeltClient.on('error_system', function (message) {
+shifterbeltClient.on('error_system', function(message) {
   console.log('There is an error: ' + message);
 });
-
-//# sourceMappingURL=app.js.map
